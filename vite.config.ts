ba79602684dc8d5530dabc14276f72823769e7cd
VoAnhBen <benva.ce190709@gmail.com>
2025-10-30 23:36:58 +0700

    update mien

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -6,9 +7,15 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
     host: 'localhost', // hoặc '0.0.0.0' nếu muốn truy cập từ máy khác
-    port: 3000         // đổi port tại đây
+    port: 3000,        // đổi port tại đây
+    
+    // Thêm dòng này để cho phép ngrok host truy cập
+    allowedHosts: [
+      'odorless-unsolved-ariah.ngrok-free.dev'
+    ]
+    
   },
-    // assetsInclude: ['**/*.xlsx'], // Handle problem file xlxs
+  // assetsInclude: ['**/*.xlsx'], // Handle problem file xlxs
   css: {
     preprocessorOptions: {
       scss: {
